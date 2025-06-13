9fbf859a76ddd6063c5b9a8d94b4b2b6ec0eff71
Stoicbuddy01 <157697264+Stoicbuddy01@users.noreply.github.com>
2025-06-13 16:38:45 +0000

    Comparisons and imp notes

---
+++ 01_basics/01_comparisons.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+/*The reason is that an equality check == and comparisons > < >= <= works differently.
+Comparisons convert NULL TO A NUMBER, treating it as 0.Thats why null >= 0 is true and null > 0 is false
+*/
